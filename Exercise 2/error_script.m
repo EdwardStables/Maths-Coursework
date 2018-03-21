e8e89d0e00dd7b20a1ce6a86b3791e4a473a6428
EdwardStables <edward.stables1198@gmail.com>
2018-03-21 13:07:05 +0000

    updated RK2 error

--- Exercise 2/error_script.m
+++ Exercise 2/error_script.m
@@ -1,35 +1,28 @@
-%Values
-R=1000; %Resistor
-C=100*10^(-9); %Capacitor
-w=20000*pi; %Frequency
-qi = 500*10^(-9); %initial charge
-h=0.000001; %step value
-ti=0.0000; tf=0.0012; %initial and final time
-T = 100*10^(-6); %Cosine period
-f = 1/T; %Cosine frequency
-
-%Equations
+r=1000;
+c=100*10^(-9);
+w=20000*pi;
+qi = 500*10^(-9);
+ti=0.000; tf=0.008;
+T = 100*10^(-6); %
+f = 1/T;
 Vin =@(t) 5*cos(2*pi*f*t);
-eqn=@(t,Vout) ((Vin(t)-Vout/c)/r);
-%To obtain the results for the 3 different RK2 methods, change the
-%commented lines in the RK2 function
-[t,y] = RK2(eqn, qi, [ti tf], h);
-Vout = y/c; %This section deals with charge, therefore the output needs to
-%be converted to voltage
+func=@(t,q) ((Vin(t)-q/c)/r);
+x=[4:1:8];
+h= 10.^(-x);
+error = zeros(1, size(h,2)); 
+for j=1:size(h,2)
+    h(j)
+    [t,y] =RK2(func,qi,[ti tf], h(j));
+    Vout = y/c;
+    exact= ((5*c*cos(w*t)+5*w*r*c^2*sin(w*t)+(qi*(1+4*pi^2)-5*c)*exp(-t/(r*c)))/((1+(w*r*c)^2)))/(100*10^-9);
+    error(j) = max(abs(exact-Vout));
+end 
+figure
+loglog((h),(error))
+title('loglog plot of error vs h')
+xlabel 'h'
+ylabel 'Error'
+plot(log(h), log(error));
+xlabel 'log(h)'
+ylabel 'log(Error)'
 
-exact= ((5*C*cos(w*t)+5*w*R*C^2*sin(w*t)+... %Exact derivation is included in the report
-    (qi*(1+4*pi^2)-5*C)*exp(-t/(R*C)))/((1+(w*R*C)^2)))/(100*10^-9);
-error=exact-Vout; %Error calculated as the difference between 
-
-%Output plotting
-figure;
-plot(t, error,'g');
-title('Error')
-xlabel 't/s'
-ylabel 'Vout/V'
-
-
-
-    
-    
-    
